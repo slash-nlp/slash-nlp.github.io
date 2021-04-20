2d5c3fa0daa2fd99fe686377f99ebe272188d542
Sowmen Rahman <sowmen.appd@gmail.com>
2021-04-20 07:38:39 +0600

    FIX: Favicon location changed for github static hosting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ module.exports = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "favicon.ico",
   organizationName: "slash-nlp", // Usually your GitHub org/user name.
   projectName: "slash-nlp", // Usually your repo name.
   themeConfig: {
